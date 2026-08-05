c04f86274d04c6d20a1a5b4d8a4e0ab6926637ca
Laura Lora <laura.lora.f@gmail.com>
2026-08-05 08:41:46 -0400

    Update Login.js

--- frontend/trabajo/src/pages/Login.js
+++ frontend/trabajo/src/pages/Login.js
@@ -1,11 +1,10 @@
 import { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
